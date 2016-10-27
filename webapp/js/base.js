e01fb392aba7b29a273794a4e04ceeed6c84eb1f
Mario Lo <Mario Lo>
2016-10-26 18:24:30 -0600

    ''

--- webapp/js/base.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    $('.btn-execute').click(function(){
-        //var user = authUser('loserpunkx182','Overstock*');
-        var pool = getUserPool();
-        console.log(pool.getCurrentUser());
-    });
-    
-}
