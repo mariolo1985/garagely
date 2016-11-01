2c2601fd71cc4540a8d8ab464b865e1a1f71b16e
mariolo1985 <loserpunkx182@gmail.com>
2016-11-01 00:45:34 -0600

    resolved credential issue

--- webservice/cognitoservice/cognitohelper.php
+++ webservice/cognitoservice/cognitohelper.php
@@ -17,11 +17,10 @@ class CognitoHelper
     function createUser($Username, $Pw, $Email){
         try{
             $sdk = new Aws\Sdk($this->sharedConfig);
-
             // GET COGNITO CLIENT
             $cognitoClient = $sdk->createCognitoIdentityProvider(
                 array(
-                    'profile' => 'garagely_lpx'
+                    'credentials' => CredentialProvider::ini('default','/var/www/html/.aws/credentials')
                 )
             );
 
@@ -42,7 +41,33 @@ class CognitoHelper
         }
 
         return $result;
-    }
+    }// end createuser
+
+    // AUTHENTICATE USER 
+    function authUser($Username,$PW){
+        try{
+            $sdk = new Aws\Sdk($this->sharedConfig);
+            $cognitoClient = $sdk->createCognitoIdentityProvider(
+                array(
+                    'credentials' => CredentialProvider::ini('default','/var/www/html/.aws/credentials')
+                )
+            );
+
+            $result = $cognitoClient->adminInitiateAuth([
+                'AuthFlow' => 'ADMIN_NO_SRP_AUTH',
+                'ClientId' =>'52d65o95sbnf1q9r49tpg349i9',
+                'UserPoolId' =>'us-west-2_Z2gGGw2Am',
+                'AuthParameters' => [
+                    'USERNAME' => $Username,
+                    'PASSWORD' => $PW
+                ]
+            ]);
+
+            return $result;
+        }catch(Exception $e){
+            return $e->getMessage();
+        }
+    }// end authuser
 
     
 }
