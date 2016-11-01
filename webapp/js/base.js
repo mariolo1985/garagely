17b753ca5649bb7d3b40ba14cea9e464bc8b2027
mariolo1985 <loserpunkx182@gmail.com>
2016-11-01 00:44:16 -0600

    Calling service

--- webapp/js/base.js
+++ webapp/js/base.js
@@ -1,8 +1,24 @@
 window.onload = function () {
-    $('.btn-execute').click(function(){
-        //var user = authUser('loserpunkx182','Overstock*');
-        var pool = getUserPool();
-        console.log(pool.getCurrentUser());
+    $('.btn-execute').click(function () {
+        $.ajax({
+            type: "POST",
+            url: "http://54.201.24.33/cognitoservice/createuser.php",
+            crossDomain:true,
+            dataType:"jsonp",
+            data:
+            {
+                username: "newtest1",
+                pw: "Password!"
+            }
+        })
+        .done(function (msg) {
+            console.log('Post result: ' + msg);
+        })
+        .fail(function(a,b,c){
+            console.log(a);
+            console.log(b);
+            console.log(c);
+        });
     });
-    
+
 }
