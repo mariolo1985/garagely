4f9d9f775d94e0f24290d0468e7851218e2124e3
mariolo1985 <loserpunkx182@gmail.com>
2016-11-05 01:00:12 -0600

    Sign up scripts

---
+++ webapp/js/signup.js
@@ -0,0 +1,54 @@
+window.onload = function () {
+
+    $('.btn-signup').click(function () {
+        var un = $('.input-login').val();
+        var pw = $('.input-pw').val();
+        var email = $('.input-email').val();
+        signUp(un, pw, email);
+    });
+}
+
+function signUp(username, pw, email) {
+    startLoader();
+    $.ajax(
+        {
+            type: "POST",
+            url: "http://54.201.24.33/cognitoservice/createuser.php",
+            data:
+            {
+                un: username,
+                pw: pw,
+                email: email
+            }
+        }
+    ).done(function (result) {
+
+        var jResult = JSON.parse(result);
+        var des = jResult['Destination'];
+        if (typeof (des) != 'undefined') {
+            // SAVE toke
+            // MOVE ON TO ANOTHER SCREEN
+            window.location = '../';
+        
+        } else {
+            // UNABLE TO CREATE USER
+        }
+
+        stopLoader();
+    })
+        .fail(function (a, b, c) {
+            console.log(a);
+            console.log(b);
+            console.log(c);
+            stopLoader();
+        });
+}
+
+
+function startLoader() {
+    $('.loader').removeClass('hide').addClass('show');
+}
+
+function stopLoader() {
+    $('.loader').removeClass('show').addClass('hide');
+}
