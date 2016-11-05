41affd41eb456c4293dc756524e0b75fdbc57098
mariolo1985 <loserpunkx182@gmail.com>
2016-11-05 00:04:13 -0600

    check for session storage

--- webapp/js/base.js
+++ webapp/js/base.js
@@ -1,24 +1,4 @@
 window.onload = function () {
-    $('.btn-execute').click(function () {
-        $.ajax({
-            type: "POST",
-            url: "http://54.201.24.33/cognitoservice/createuser.php",
-            crossDomain:true,
-            dataType:"jsonp",
-            data:
-            {
-                username: "newtest1",
-                pw: "Password!"
-            }
-        })
-        .done(function (msg) {
-            console.log('Post result: ' + msg);
-        })
-        .fail(function(a,b,c){
-            console.log(a);
-            console.log(b);
-            console.log(c);
-        });
-    });
-
+    var toke = sessionStorage.getItem('toke');
+    console.log(toke);
 }
