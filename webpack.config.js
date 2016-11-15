e70b2af3603c94eb25e1f3c7a556da710110ab09
mariolo1985 <loserpunkx182@gmail.com>
2016-11-15 00:06:52 -0700

    react packages and transpile config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        review: [
+            './js/review.js'
+        ],
+        index: [
+            './js/index.js'
+        ],
+        history: [
+            './js/history.js'
+        ]
+    },
+    output: {
+        path: __dirname + '/build/webpackd',
+        filename: "[name]_bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        })
+    ]
+
+};
