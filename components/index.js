9219cfb968017936cb474329ddeba43e220fa1aa
mariolo1985 <loserpunkx182@gmail.com>
2016-11-24 23:16:27 -0700

    React component root

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export Modal from './comp_modal/Modal';
+export ModalHead from './comp_modal/ModalHead';
+export ModalBody from './comp_modal/ModalBody';
+export ModalFooter from './comp_modal/ModalFooter';
