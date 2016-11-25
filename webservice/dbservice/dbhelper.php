dc67fce0148954a253a433ab8c243a10eee235d2
mariolo1985 <loserpunkx182@gmail.com>
2016-11-24 23:22:23 -0700

    DB services to call stored procedures

--- webservice/dbservice/dbhelper.php
+++ webservice/dbservice/dbhelper.php
@@ -11,9 +11,114 @@ class DbHelper{
         $_mysqli = @new mysqli("devgaragely.cjh3odacflwh.us-west-2.rds.amazonaws.com:3306",$cred['un'],$cred['pw'],"devgaragely_db");
         if ($_mysqli->connect_error){
             echo 'Connection Error: ' . $_mysqli->connect_error;
+            return null;
         }
 
         return $_mysqli;
+    }// end getdefaultconnections
+
+    // NEW USER
+    function insertUser($conn, $username, $email){
+        try{
+            $stmt = $conn->prepare('SET @_un := ?');// set username
+            $stmt->bind_param('s',$username);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_email := ?');// SET EMAIL
+            $stmt->bind_param('s', $email);
+            $stmt->execute();
+
+            $result = $conn->query('CALL pNewUser(@_un,@_email)');
+            return $result;
+            $result->free();
+        }catch(Exception $e){
+            echo $e->getMessage(); // for debugging 
+        }
+        $conn->close();
+    }// end insertUser
+
+    // INSERT USER LOC
+    function insertUserLoc($conn, $username,$addressline1, $addressline2, $city, $state, $zip, $latlng){
+
+        try{
+            $stmt = $conn->prepare('SET @_un := ?');
+            $stmt->bind_param('s',$username);
+            $stmt->execute();
+            
+            $stmt = $conn->prepare('SET @_addressline1 := ?');
+            $stmt->bind_param('s',$addressline1);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_addressline2 := ?');
+            $stmt->bind_param('s',$addressline2);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_city := ?');
+            $stmt->bind_param('s',$city);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_state := ?');
+            $stmt->bind_param('s',$state);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_zip := ?');
+            $stmt->bind_param('s',$zip);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_latlng := ?');
+            $stmt->bind_param('s',$latlng);
+            $stmt->execute();
+
+            $result = $conn->query('CALL pInsertUserLocWithLatlng(@_un, @_addressline1, @_addressline2, @_city, @_state, @_zip, @_latlng)');
+            return $result;
+            $result->free();
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+
+        $conn->close();
+
+    }// end insertUserLoc
+
+    // UPDATES USER LOC
+    function updateUserLoc($conn, $username,$addressline1, $addressline2, $city, $state, $zip, $latlng){
+        try{
+            $stmt = $conn->prepare('SET @_un := ?');
+            $stmt->bind_param('s',$username);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_addressline1 := ?');
+            $stmt->bind_param('s',$addressline1);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_addressline2 := ?');
+            $stmt->bind_param('s',$addressline2);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_city := ?');
+            $stmt->bind_param('s',$city);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_state := ?');
+            $stmt->bind_param('s',$state);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_zip := ?');
+            $stmt->bind_param('s',$zip);
+            $stmt->execute();
+
+            $stmt = $conn->prepare('SET @_latlng := ?');
+            $stmt->bind_param('s',$latlng);
+            $stmt->execute();
+
+            $result = $conn->query('CALL pUpdateUserLocWithLatlng(@_un, @_addressline1, @_addressline2, @_city, @_state, @_zip, @_latlng)');
+            return $result;
+            $result->free();
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+
+        $conn->close();
     }
 }
 ?>
--- webservice/dbservice/test.php
+++ webservice/dbservice/test.php
@@ -4,6 +4,16 @@ include "dbhelper.php";
 $_dbhelper = new DbHelper();
 $conn = $_dbhelper->getDefaultConnection();
 
-echo $conn->client_info;
-echo $conn->server_info;
+echo file_exists('../cognitoservice/cognitohelper.php')? "File exist": "FIle does not exist";
+/*
+if ($conn != null){
+    
+    //$result = $_dbhelper->insertUser($conn,"newtest1","loserpunkx182@gmail.com");
+    $result = $_dbhelper->updateUserLoc($conn, "newtest1","321 Street", "line 2", "city", "State", "zip", "latlng there");
+
+    if ($result){
+        echo 'Insert success';
+    }
+}
+*/
 ?>
