49441ff9205b77c9309eb33ef17fb604aec9f674
mariolo1985 <loserpunkx182@gmail.com>
2016-11-24 23:18:23 -0700

    Add marker, custom marker, hide default map UI, marker click event

--- webapp/js/gmaps.js
+++ webapp/js/gmaps.js
@@ -1,23 +1,100 @@
-//AIzaSyCKDC3HPE7EOYsOSclRHBwOiV2Z-9rfocg
-
 function loadMapsApi() {
     var p = document.location.protocol;
     var s = document.createElement('script');
-    s.setAttribute('src', p + '//maps.googleapis.com/maps/api/js?key=AIzaSyCKDC3HPE7EOYsOSclRHBwOiV2Z-9rfocg&callback=initMap');
+    s.setAttribute('src', p + '//maps.googleapis.com/maps/api/js?key=AIzaSyDOf-gDXTDEamqvKeUe913CgCrHBJhtE7I&callback=mapScriptLoaded');
     s.setAttribute('async', '');
     s.setAttribute('defer', '');
     document.body.appendChild(s);
 }
 
 var map;
-function initMap() {
-    var latlng = {
+function mapScriptLoaded() {
+    // JUST A CALLBACK
+    var gpsLoc = {
         lat: 40.5941989,
         lng: -111.8592
     };
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: latlng,
-        zoom: 12
+        center: gpsLoc,
+        zoom: 12,
+        disableDefaultUI:true
     });
+
+}
+
+function getAddressLatLng(address) {
+    var mGeocoder = new google.maps.Geocoder();
+    mGeocoder.geocode(
+        {
+            'address': address
+        },
+        function(results, status) {
+            console.log(results);
+            if (status == 'OK') {
+
+                if (results.length == 1) {
+                    // HAVE 1 RESULTS
+                    getUserMarker(results[0].geometry.location);
+                } else if (results.length > 1) {
+                    // MORE THAN 1 RESULT        
+                    getUserMarker(results[0].geometry.location);// FIX ME
+                } else {
+                    // NO RESULTS
+                    return null;
+                }
+            }
+        }
+    );
+
+}
+
+// GETS THE ADDRESS AS latlng
+// MAY HAVE MULTIPLE RESULTS
+function getModalAddress() {
+    console.log('getmodaladdress');
+    var addressLine1 = "",
+        addressLine2 = "",
+        city = "",
+        state = "",
+        zip = "";
+
+    addressLine1 = $('.txb-address-line1').val();
+    addressLine2 = $('.txb-address-line2').val();
+    city = $('.txb-city').val();
+    state = $('.state-item.selected').html();
+    zip = $('.txb-zip').val();
+
+    var addressStr = "";
+    addressStr += addressLine1 + ' ';
+    addressStr += addressLine2 + ' ';
+    addressStr += city + ' ';
+    addressStr += state + ' ';
+    addressStr += zip + ' ';
+
+    // GET ADDRESS    
+    getAddressLatLng(addressStr);
+}
+
+// SHOW RESULTS AS MARKERS
+var userMarker;// IMPORTANT
+function getUserMarker(userLocationObj) {
+    userMarker = new google.maps.Marker({
+        position: userLocationObj,
+        map: map,
+        title: 'Your Location',
+        icon: {
+            url: './images/car_marker.png',
+            scaledSize: new google.maps.Size(25, 25)
+        }
+    });
+
+    var windowMarkup = "<div>Window here</div>";
+    var infoWindow = new google.maps.InfoWindow({
+        content: windowMarkup
+    });
+    userMarker.addListener('click', function() {
+        infoWindow.open(map, userMarker);
+    })
 
 }
