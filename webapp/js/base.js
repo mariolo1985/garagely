cd8a2b60bc6f6a40ec71783d887e7ab1ac84c912
mariolo1985 <loserpunkx182@gmail.com>
2016-11-24 23:29:40 -0700

    handle hasaddress

--- webapp/js/base.js
+++ webapp/js/base.js
@@ -1,1 +1,43 @@
 // BASE FUNCTIONS
+
+// AUTHO HELPERS
+function handleToke(callbackHandler) {    
+    var toke = getToke();
+
+    if (toke != null) {
+        $.ajax({
+            url: 'http://54.201.24.33/cognitoservice/getuser.php',
+            type: 'POST',
+            data: {
+                toke: toke
+            }
+        }).done(function (result) {
+            var jResult = JSON.parse(result);
+            console.log(jResult);
+            jResult.forEach(function (item, i) {
+                switch (item['Name']) {
+                    case "custom:HasAddress":
+                        var attrVal = item['Value'];
+                        var hasAddress = attrVal === 'TRUE' ? true : false;
+                        callbackHandler(hasAddress);
+                        if (!hasAddress) {
+                            //  POPUP ADDRESS
+                            console.log('no address');
+                            
+                        } else {
+                            // HAVE ADDRESS
+                            console.log('have address');
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+            });
+
+        }).fail(function (a, b, c) {
+            console.log('failed');
+        })
+    }
+}
