728e62551be6044b3940235aad5050965fc0d331
mariolo1985 <loserpunkx182@gmail.com>
2016-11-26 16:31:50 -0700

    check email

--- webservice/cognitoservice/createuser.php
+++ webservice/cognitoservice/createuser.php
@@ -1,33 +1,44 @@
 <?php
     include 'cognitohelper.php';
-    $result = null;
+    include '../dbservice/dbhelper.php';    
 
     try{
         if ((isset($_POST['un'])) && (isset($_POST['pw'])) && (isset($_POST['email']))) {
             $username = $_POST['un'];
             $pw = $_POST['pw'];
             $email = $_POST['email'];
-            $cognitoHelper = new CognitoHelper();
-            $result = $cognitoHelper->createUser($username,$pw,$email);
 
+            // CHECK IF EMAIL EXIST            
+            $_dbhelper = new DbHelper();
+            $conn = $_dbhelper->getDefaultConnection();
+            $result = $_dbhelper->checkEmail($conn,$email);         
+
+            if (count($result) > 0){
+                // EMAIL ALREADY EXIST 
+                echo "EmailExistException";
+                return;
+            }            
+            
+            // CREATE USER IN COGNITO
+            $_cognitoHelper = new CognitoHelper();
+            $result = $_cognitoHelper->createUser($username,$pw,$email);
+            
             if ($result == 'UsernameExistsException') {
                 echo $result;
+
             }else if ($result == 'InvalidPasswordException'){                    
                 echo $result;
             }else{
-                // add user to db (dbhelper.php) : -FIX ME 
-                include '../dbservice/dbhelper.php';
-                $_dbhelper = new DbHelper();
-                $conn = $_dbhelper->getDefaultConnection();
-
+                // ADD USER TO DB                              
                 if ($conn !=null){
+                    $conn->next_result();
                     $insertUserResult = $_dbhelper->insertUser($conn, $username, $email);                    
                 }
 
                 echo json_encode($result['CodeDeliveryDetails']);
             }
       
-   
+            $conn->close();// CLOSE CONNECTION
         }
     }catch(Exception $e){
         return $e->getMessage();
