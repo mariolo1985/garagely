1c30fab63f17a53774bae0c0041ff7918f64f154
mariolo1985 <loserpunkx182@gmail.com>
2016-11-26 16:33:11 -0700

    add nav

--- components/index.js
+++ components/index.js
@@ -1,4 +1,1 @@
-export Modal from './comp_modal/Modal';
-export ModalHead from './comp_modal/ModalHead';
-export ModalBody from './comp_modal/ModalBody';
-export ModalFooter from './comp_modal/ModalFooter';
+export Nav from './comp_nav/Nav';
